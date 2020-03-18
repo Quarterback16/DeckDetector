47bd8aade6a7232fbd23d07e5dffe1960038a3c0
Steve Colonna <quarterback16@grapevine.com.au>
2020-03-18 12:55:53 +1100

    Add RunnOps to solution

---
+++ dd/Run-DD.bat
@@ -0,0 +1,25 @@
+::  DD (.NET framework 4.6.1 Console App)
+::
+::  dd -r c  Championship report
+::  dd -r l  Last Month Champs Report 
+::  dd -r t  This Month Champs Report 
+::  dd -r f  Frequency Report
+::  dd -r a  Alpha Report 
+::  dd -r d  Home Deck Report 
+::
+::set HOMEDECK=T Galakrond Warrior
+::set HOMEDECK=HSR Galakrond Warrior
+::set HOMEDECK=HSR Pirate Warrior
+::set HOMEDECK=HSR Token Druid
+::set HOMEDECK=HSR Quest Priest
+::set HOMEDECK=HSR Galakrond Shaman
+::set HOMEDECK=HSR Secret Highlander Hunter
+::set HOMEDECK=TS Highlander Hunter
+::set HOMEDECK=VS Highlander Hunter
+::set HOMEDECK=VS Quest Hunter
+set HOMEDECK=HTD Dragon Hunter
+::set HOMEDECK=WS Dragon Hunter
+::set HOMEDECK=WS Control Shaman
+::set HOMEDECK=HSR Mech Paladin
+::set HOMEDECK=VS Galakrond Rogue
+dd.exe
